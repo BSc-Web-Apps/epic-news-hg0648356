3a3e79662e12b3ed15b22b0b0cab2a851b673f26
hg0648356 <0648356@hc.ac.je>
2025-11-13 09:48:11 +0000

    first team member - img not working

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
 import portrait1 from '~/assets/jpg/hairybiker.jpg'
+import headshot from './assets/headshot.webp'
+import ShareButtons from './components/molecules/SocialMediaButtons.tsx'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -23,18 +25,25 @@ export default function App() {
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Your Journey Begins!</h1>
-						<p className="text-base text-gray-600 md:text-lg lg:text-xl">
-							Welcome to Epic News, where the latest developments in tech are
-							found.
-						</p>
-						<button className="h-2/10 w-1/2 rounded-lg bg-blue-700 p-1 font-semibold hover:cursor-pointer hover:bg-blue-900 hover:underline hover:underline-offset-2 sm:h-3/10 sm:text-sm md:h-4/10 md:text-2xl lg:h-5/10 lg:text-5xl">
-							Learn More
-						</button>
-						<div className="grid grid-cols-1 grid-rows-4 bg-gray-800 p-1">
-							<img src={portrait1} />
-							<div> Geoff Bingle</div>
-							<div> Designer in Chief</div>
-							<div> linkedin x</div>
+						<div className="w-fit rounded-lg bg-slate-800 p-8">
+							<img
+								src={portrait1}
+								alt="An employee"
+								className="mx-auto h-64 w-64 rounded-full"
+							/>
+
+							<div className="pt-6">
+								<h3 className="font-semi-bold text-center text-white">
+									Leonard Krasner
+								</h3>
+								<p className="pt-1 text-center text-slate-400">
+									Senior Designer
+								</p>
+
+								<div className="flex justify-center gap-4 pt-6 text-slate-400">
+									<ShareButtons />
+								</div>
+							</div>
 						</div>
 					</main>
 				</div>
