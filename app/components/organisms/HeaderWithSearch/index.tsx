990d9e254a724ea547065f3298acceb6aefe3b93
hg0648356 <0648356@hc.ac.je>
2025-11-27 10:31:08 +0000

    header

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
 import { useOptionalUser } from '#app/utils/user.ts'
-import epiclogo from '~/assets/jpg/Screenshot 2025-10-16 100113.png'
+import epicLogo from '~/assets/png/epic-news-logo.png'
 
 export default function HeaderWithSearch() {
 	const matches = useMatches()
@@ -12,11 +12,31 @@ export default function HeaderWithSearch() {
 	const user = useOptionalUser()
 
 	return (
-		<header className="container py-6">
-			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<div>
-					<img alt="logo" src={epiclogo} className="h-15 w-20" />
+		<header className="bg-primary container py-6">
+			<nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+				<Link to="/">
+					<div className="flex items-center gap-4">
+						<img src={epicLogo} alt="Epic News Logo" className="w-16" />
+						<span className="text-foreground text-sm">Epic News</span>
+					</div>
+				</Link>
+
+				<div className="flex flex-1 justify-center gap-8">
+					<Link
+						to="/news"
+						className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
+					>
+						News
+					</Link>
+					<Link
+						to="/about-us"
+						prefetch="intent"
+						className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
+					>
+						About us
+					</Link>
 				</div>
+
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
 				</div>
--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,14 @@
 import { RiLinkedinBoxFill, RiTwitterXFill } from 'react-icons/ri'
-import { useLoaderData } from 'react-router'
+import { Outlet, useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
+import FooterBasic from './components/organisms/Footer/FooterBasic.tsx'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 import Document from './components/shared-layout/Document.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
-import HeaderWithSearch from './components/organisms/HeaderWithSearch'
-import FooterBasic from './components/organisms/Footer/FooterBasic.tsx'
 import portrait1 from '~/assets/jpg/hairybiker.jpg'
 import portrait2 from '~/assets/jpg/nintchdbpict000338874152.jpg'
 
@@ -56,7 +56,7 @@ export default function App() {
 			<div className="flex h-screen flex-col justify-between">
 				<HeaderWithSearch />
 				<div className="flex-1">
-					<main className="grid h-full place-items-center">
+					{/* <main className="grid h-full place-items-center">
 						<h1 className="text-mega">Epic News</h1>
 						<h2 className="text-2xl">harry gee news read now</h2>
 						<div className="m-4 flex gap-4">
@@ -78,7 +78,8 @@ export default function App() {
 								imageSrc={portrait2}
 							></TeamMemberCard>
 						</div>
-					</main>
+					</main> */}
+					<Outlet />
 				</div>
 				<div className="container flex justify-between pb-5">
 					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
