e45667266c90b2de905d1dd0a48ca826ab1130a4
hg0648356 <0648356@hc.ac.je>
2025-12-04 12:07:38 +0000

    prisma, security, articles
    finished up until challenge of 'load and display article data'

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,13 @@
-import { RiLinkedinBoxFill, RiTwitterXFill } from 'react-icons/ri'
 import { Outlet, useLoaderData } from 'react-router'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import FooterBasic from './components/organisms/Footer/FooterBasic.tsx'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 import Document from './components/shared-layout/Document.tsx'
+import { useToast } from './components/toaster.tsx'
+import { EpicToaster } from './components/ui/sonner.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
@@ -20,20 +22,31 @@ export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
 	const theme = useTheme()
+	useToast(data?.toast)
 
 	return (
-		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
-			<div className="flex h-screen flex-col justify-between">
-				<HeaderWithSearch />
-				<div className="flex-1">
-					<Outlet />
+		<AuthenticityTokenProvider token={data?.csrfToken ?? ''}>
+			<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
+				<div className="flex h-screen flex-col justify-between">
+					<HeaderWithSearch />
+					<div className="flex-1">
+						<Outlet />
+					</div>
+					<div className="container flex justify-between pb-5">
+						<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
+					</div>
+					<FooterBasic />
 				</div>
-				<div className="container flex justify-between pb-5">
-					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
-				</div>
-				<FooterBasic />
-			</div>
-		</Document>
+				<EpicToaster
+					closeButton
+					position="bottom-right"
+					theme={theme}
+					expand
+					richColors
+					duration={5000}
+				/>
+			</Document>
+		</AuthenticityTokenProvider>
 	)
 }
 
--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,20 +1,39 @@
 import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, data, useLoaderData } from 'react-router'
 import { toTitleCase } from '~/utils/stringUtils.ts'
+import { prisma } from '~/utils/db.server.ts'
 
 export async function loader({ params }: LoaderFunctionArgs) {
 	const { category } = params
 	invariant(typeof category === 'string', 'Category not found')
 	const categoryTitle = toTitleCase(category)
 
-	return data({ categoryTitle })
+	const allArticles = await prisma.article.findMany({
+		select: {
+			id: true,
+			title: true,
+			category: { select: { name: true } },
+			images: { select: { id: true } },
+		},
+	})
+
+	return data({ categoryTitle, allArticles })
 }
 
 export default function NewsCategoryPage() {
-	const { categoryTitle } = useLoaderData<typeof loader>()
+	const { categoryTitle, allArticles } = useLoaderData<typeof loader>()
 	return (
 		<div className="container py-16">
-			<h2 className="text-h2">{categoryTitle}</h2>
+			<h2 className="text-h2 mb-8">{categoryTitle}</h2>
+
+			<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-5">
+				{allArticles.map((article) => (
+					<div key={article.id} className="bg-red-900 p-4">
+						<h3>{article.title}</h3>
+						<p>{article.category?.name || 'General News'}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
